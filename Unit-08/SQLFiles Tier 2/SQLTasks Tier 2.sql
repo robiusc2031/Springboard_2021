11c505455f3bbeb19e0841bdb55a84500ce1832f
Robi <69650519+robiusc2031@users.noreply.github.com>
2021-08-06 04:38:49 -0400

    Update SQLTasks Tier 2.sql

--- Unit-08/SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ Unit-08/SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -80,15 +80,22 @@ more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
 CODE:
-
-ANSWER:
+SELECT `name` , `monthlymaintenance` ,
+CASE WHEN `monthlymaintenance` <100
+THEN 'cheap'
+ELSE 'expensive'
+END AS affordability
+FROM `Facilities`;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
 CODE:
+SELECT `firstname` , `surname`
+FROM `Members`
+WHERE `joindate` = (SELECT MAX(joindate) FROM Members);
 
-ANSWER:
+ANSWER: Darren Smith
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
